eb1c63c36183742d2b0d4850ebe815403326b6a9
Kholiqm <kholiqcode@gmail.com>
2021-03-11 18:01:01 +0700

    Update Database

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -2,11 +2,10 @@
 
 namespace Database\Factories;
 
-use App\Models\Address;
 use App\Models\Order;
 use App\Models\Seller;
 use App\Models\User;
-use Illuminate\Support\Str;
+use App\Models\Village;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class OrderFactory extends Factory
@@ -29,7 +28,10 @@ class OrderFactory extends Factory
             'id' => $this->faker->uuid,
             'user_id' => User::factory(),
             'seller_id' => Seller::factory(),
-            'address_id' => Address::factory(),
+            'shipping_method' => $this->faker->randomElement([0, 1]),
+            'village_id' => Village::all()->random()->id,
+            'customer_addressName' => $this->faker->randomElement(['Home', 'Office', 'Hotel', 'Kost']),
+            'customer_address' => $this->faker->streetAddress,
             'order_at' => $this->faker->dateTimeBetween('-1 years', now()),
             'expire_at' => $this->faker->dateTimeInInterval(now(), '+1 days'),
             'status' => $this->faker->randomElement([0, 1, 2, 3, 4, 5])
--- database/migrations/2021_02_19_150809_create_orders_table.php
+++ database/migrations/2021_02_19_150809_create_orders_table.php
@@ -17,7 +17,10 @@ class CreateOrdersTable extends Migration
             $table->uuid('id')->primary();
             $table->foreignUuid('user_id')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
             $table->foreignUuid('seller_id')->constrained('sellers')->onUpdate('cascade')->onDelete('cascade');
-            $table->foreignUuid('address_id')->nullable()->constrained('addresses')->onUpdate('cascade')->onDelete('cascade');
+            $table->unsignedTinyInteger('shipping_method')->default(0)->comment('0=Diantar,1=Ambil Sendiri');
+            $table->foreignId('village_id')->nullable()->constrained('villages')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('customer_addressName')->nullable();
+            $table->string('customer_address')->nullable();
             $table->timestamp('order_at');
             $table->timestamp('expire_at');
             $table->unsignedTinyInteger('status')->default(0)->comment('0=Menunggu Pembayaran,1=Konfirmasi Seller,2=Diproses,3=Dikirim,4=Diterima,5=Review,6=Selesai,7=Dibatalkan');
